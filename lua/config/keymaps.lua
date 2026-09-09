dd6cc0cdf49e7cf62c45b5d497d41554ccb70963
Szymon Drosdzol <szymon@doyensec.com>
2026-09-09 22:12:57 +0200

    Keymap to copy the current files path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Custom keymaps
+
+vim.keymap.set("n", "<leader>yr", function()
+  local root = LazyVim.root()
+  local file = vim.api.nvim_buf_get_name(0)
+  local path = vim.fn.fnamemodify(file, ":.")
+  if root and file:find("^" .. vim.pesc(root)) then
+    path = file:sub(#root + 2)
+  end
+  vim.fn.setreg("+", path)
+  vim.notify(path)
+end, { desc = "Yank project-relative file path" })
