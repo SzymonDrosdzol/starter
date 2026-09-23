83f4679b8516aa8f7dd89776be81c3d3b3cfb42f
Szymon Drosdzol <szymon@doyensec.com>
2026-09-24 01:30:41 +0200

    Fix coloring of unnecessary code

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -3,6 +3,12 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "storm",
+      on_highlights = function(hl, c)
+        hl.DiagnosticUnnecessary = {
+          undercurl = true,
+          sp = c.comment,
+        }
+      end,
     },
   },
 }
